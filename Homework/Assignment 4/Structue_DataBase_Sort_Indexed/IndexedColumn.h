378b2a4f8322f353867ca284c7a22d5250949b60
joseroman1 <joseroman@outlook.com>
2015-10-08 22:47:21 -0700

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Homework/Assignment 4/Structue_DataBase_Sort_Indexed/IndexedColumn.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   IndexedColumn.h
+ * Author: Jose Roman
+ * Created on October 6, 2015, 11:20 AM
+ */
+
+#ifndef INDEXEDCOLUMN_H
+#define	INDEXEDCOLUMN_H
+
+struct IndxCol{
+    int size;
+    int *array;
+    int *indx;
+};
+
+#endif	/* INDEXEDCOLUMN_H */
