7cff1ae5265f95832a333f1b8528b634d1036905
joseroman1 <joseroman@outlook.com>
2015-12-08 12:35:11 -0800

    Signed-off-by: joseroman1 <joseroman@outlook.com>

---
+++ Book/Simple_Vector/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on December 8, 2015, 12:00 PM
+ */
+
+//
+#include <iostream>
+using namespace std;
+
+//User Libraries
+#include "SimpleVector.h"
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
